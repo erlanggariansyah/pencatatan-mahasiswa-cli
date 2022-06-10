dbe5460b24018ba20ac6699c31f291d23d18a152
Erlangga Riansyah <erlanggariansyah0@gmail.com>
2022-06-10 10:26:46 +0700

    Main class documentation

--- src/com/huyai/Main.java
+++ src/com/huyai/Main.java
@@ -2,10 +2,18 @@ package com.huyai;
 
 import com.huyai.services.Menu;
 
-
+/**
+ * Main class
+ */
 public class Main {
+    /**
+     * static void main function, run the applications
+     */
     public static void main(String[] args) {
+        // Instansiasi class menu untuk memanggil function showMenu
         Menu menu = new Menu();
+
+        // memanggil function showMenu untuk menampilkan list menu ke console
         menu.showMenu();
     }
 }
