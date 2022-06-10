e1ae7095e1627342d7d73e88d2f7e8f0b81e48a3
Erlangga Riansyah <erlanggariansyah0@gmail.com>
2022-06-10 10:27:53 +0700

    DB class documentation

--- src/com/huyai/Database.java
+++ src/com/huyai/Database.java
@@ -4,6 +4,10 @@ import com.huyai.model.Mahasiswa;
 
 import java.util.ArrayList;
 
+/**
+ * class database, class yang digunakan untuk menampung data mahasiswa
+ */
 public class Database {
+    // static variable arraylist, menampung array yang berisi object mahasiswa
     public static ArrayList<Mahasiswa> DbMahasiswa = new ArrayList<>();
 }
