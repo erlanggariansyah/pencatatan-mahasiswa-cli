d23b7becb950fa3f960fdb907c0d6c95c6b23802
Erlangga Riansyah <erlanggariansyah0@gmail.com>
2022-06-10 10:56:30 +0700

    Person interface documentation

--- src/com/huyai/model/PersonInterface.java
+++ src/com/huyai/model/PersonInterface.java
@@ -1,5 +1,10 @@
 package com.huyai.model;
 
+/**
+ * interface yang dibuat untuk mengimplementasikan class Person
+ */
 public interface PersonInterface {
+    // fungsi bawaan selfIntroduce akan selalu ada didalam setiap child implemented class
+    // sebagai penggambaran overriding polymorphism
     String selfIntroduce();
 }
